aba099ce9f512f2efa8c156d71534b7c745be83b
Meet Barevadiya <meetbarevadiya8@gmail.com>
2025-07-16 19:36:49 +0530

    Add Search & Sort Features

--- sweetshop/store/views.py
+++ sweetshop/store/views.py
@@ -5,13 +5,35 @@ from .forms import SweetForm
 
 # Create your views here.
 
-
+#Sweet List Show and Search Filters
 def sweet_list(request):
     sweets = Sweet.objects.all()
-    form = SweetForm()  # ← ADD THIS
+    form = SweetForm()
+
+    #  Search Filters
+    name_query = request.GET.get('name', '')
+    category_query = request.GET.get('category', '')
+    min_price = request.GET.get('min_price', '')
+    max_price = request.GET.get('max_price', '')
+
+    if name_query:
+        sweets = sweets.filter(name__icontains=name_query)
+    if category_query:
+        sweets = sweets.filter(category__icontains=category_query)
+    if min_price:
+        sweets = sweets.filter(price__gte=min_price)
+    if max_price:
+        sweets = sweets.filter(price__lte=max_price)
+
     return render(request, 'index.html', {
         'sweets': sweets,
-        'form': form  
+        'form': form,
+        'filters': {
+            'name': name_query,
+            'category': category_query,
+            'min_price': min_price,
+            'max_price': max_price,
+        }
     })
 
 # Add Sweet
